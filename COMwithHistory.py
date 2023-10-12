ab3837ed158434d2cc7a3a14a8574ae58911c49a
pointm <2765211704@qq.com>
2023-10-12 23:24:04 +0800

    参数化建模与扫参最终版本

--- COMwithHistory.py
+++ COMwithHistory.py
@@ -280,12 +280,12 @@ projectName = fullname
 # 下面的被调用的几个方法都隶属于VBA Application Object，
 # VBA Application Object隶属的方法会直接使用刚刚的cst的COM对象进行调用
 # cst.SetQuietMode(True)
-# new_mws = cst.NewMWS()  # 新建并且返回一个微波工作室的对象
+new_mws = cst.NewMWS()  # 新建并且返回一个微波工作室的对象
 # 这个返回的对象跟直接打开某一个文件返回的对象是一样的
-new_mws = cst.OpenFile(fullname + '.cst')
+# new_mws = cst.OpenFile(fullname + '.cst')
 mws = cst.Active3D()  # 控制现在的被打开的微波工作室
 
-# CstSaveAsProject(mws, projectName)
+CstSaveAsProject(mws, projectName)
 
 # 工作频率设置
 sCommand = ['Solver.FrequencyRange "%f","%f"' % (Frq[0], Frq[1])]
@@ -322,3 +322,5 @@ PickPort2(mws)
 
 # 开始扫参
 ParaSweep(mws)
+
+#
