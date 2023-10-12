f4fa48a865f4b26a126db991829898ab9a45bab6
pointm <2765211704@qq.com>
2023-10-12 21:03:21 +0800

    参数化建模已经实现

--- COMwithHistory.py
+++ COMwithHistory.py
@@ -2,6 +2,12 @@ import win32com.client
 import os
 
 
+def quotes_to_list(text):
+    lines = text.splitlines()
+    quoted_lines = ["" + line.lstrip() + "" for line in lines]
+    return quoted_lines
+
+
 def AddToHistory(mws, Tag, Command):
     '''
     添加到历史树
@@ -49,9 +55,9 @@ def BrickBuild(mws, Tag, Name, component, material, Xrange, Yrange, Zrange):
                 '.Name "%s"' % Name,
                 '.Component "%s"' % component,
                 '.Material "%s"' % material,
-                '.Xrange "%f", "%f"' % (Xrange[0], Xrange[1]),
-                '.Yrange "%f", "%f"' % (Yrange[0], Yrange[1]),
-                '.Zrange "%f", "%f"' % (Zrange[0], Zrange[1]),
+                '.Xrange "%s", "%s"' % (Xrange[0], Xrange[1]),
+                '.Yrange "%s", "%s"' % (Yrange[0], Yrange[1]),
+                '.Zrange "%s", "%s"' % (Zrange[0], Zrange[1]),
                 '.Create',
                 'End With']
     AddToHistory(mws, Tag, sCommand)
@@ -115,6 +121,134 @@ def PickPort2(mws):
     AddToHistory(mws, 'define port 2', sCommand)
 
 
+def FreqSimulation(mws):
+    Tag = 'StartSolver'
+    Command = """
+    Mesh.SetCreator "High Frequency" 
+
+    With FDSolver
+        .Reset 
+        .SetMethod "Tetrahedral", "General purpose" 
+        .OrderTet "Second" 
+        .OrderSrf "First" 
+        .Stimulation "All", "All" 
+        .ResetExcitationList 
+        .AutoNormImpedance "False" 
+        .NormingImpedance "50" 
+        .ModesOnly "False" 
+        .ConsiderPortLossesTet "True" 
+        .SetShieldAllPorts "False" 
+        .AccuracyHex "1e-6" 
+        .AccuracyTet "1e-4" 
+        .AccuracySrf "1e-3" 
+        .LimitIterations "False" 
+        .MaxIterations "0" 
+        .SetCalcBlockExcitationsInParallel "True", "True", "" 
+        .StoreAllResults "False" 
+        .StoreResultsInCache "False" 
+        .UseHelmholtzEquation "True" 
+        .LowFrequencyStabilization "True" 
+        .Type "Auto" 
+        .MeshAdaptionHex "False" 
+        .MeshAdaptionTet "True" 
+        .AcceleratedRestart "True" 
+        .FreqDistAdaptMode "Distributed" 
+        .NewIterativeSolver "True" 
+        .TDCompatibleMaterials "False" 
+        .ExtrudeOpenBC "False" 
+        .SetOpenBCTypeHex "Default" 
+        .SetOpenBCTypeTet "Default" 
+        .AddMonitorSamples "True" 
+        .CalcPowerLoss "True" 
+        .CalcPowerLossPerComponent "False" 
+        .StoreSolutionCoefficients "True" 
+        .UseDoublePrecision "False" 
+        .UseDoublePrecision_ML "True" 
+        .MixedOrderSrf "False" 
+        .MixedOrderTet "False" 
+        .PreconditionerAccuracyIntEq "0.15" 
+        .MLFMMAccuracy "Default" 
+        .MinMLFMMBoxSize "0.3" 
+        .UseCFIEForCPECIntEq "True" 
+        .UseEnhancedCFIE2 "True" 
+        .UseFastRCSSweepIntEq "false" 
+        .UseSensitivityAnalysis "False" 
+        .UseEnhancedNFSImprint "False" 
+        .RemoveAllStopCriteria "Hex"
+        .AddStopCriterion "All S-Parameters", "0.01", "2", "Hex", "True"
+        .AddStopCriterion "Reflection S-Parameters", "0.01", "2", "Hex", "False"
+        .AddStopCriterion "Transmission S-Parameters", "0.01", "2", "Hex", "False"
+        .RemoveAllStopCriteria "Tet"
+        .AddStopCriterion "All S-Parameters", "0.01", "2", "Tet", "True"
+        .AddStopCriterion "Reflection S-Parameters", "0.01", "2", "Tet", "False"
+        .AddStopCriterion "Transmission S-Parameters", "0.01", "2", "Tet", "False"
+        .AddStopCriterion "All Probes", "0.05", "2", "Tet", "True"
+        .RemoveAllStopCriteria "Srf"
+        .AddStopCriterion "All S-Parameters", "0.01", "2", "Srf", "True"
+        .AddStopCriterion "Reflection S-Parameters", "0.01", "2", "Srf", "False"
+        .AddStopCriterion "Transmission S-Parameters", "0.01", "2", "Srf", "False"
+        .SweepMinimumSamples "3" 
+        .SetNumberOfResultDataSamples "1001" 
+        .SetResultDataSamplingMode "Automatic" 
+        .SweepWeightEvanescent "1.0" 
+        .AccuracyROM "1e-4" 
+        .AddSampleInterval "", "", "1", "Automatic", "True" 
+        .AddSampleInterval "", "", "", "Automatic", "False" 
+        .MPIParallelization "False"
+        .UseDistributedComputing "False"
+        .NetworkComputingStrategy "RunRemote"
+        .NetworkComputingJobCount "3"
+        .UseParallelization "True"
+        .MaxCPUs "1024"
+        .MaximumNumberOfCPUDevices "2"
+    End With
+
+    With IESolver
+        .Reset 
+        .UseFastFrequencySweep "True" 
+        .UseIEGroundPlane "False" 
+        .SetRealGroundMaterialName "" 
+        .CalcFarFieldInRealGround "False" 
+        .RealGroundModelType "Auto" 
+        .PreconditionerType "Auto" 
+        .ExtendThinWireModelByWireNubs "False" 
+        .ExtraPreconditioning "False" 
+    End With
+
+    With IESolver
+        .SetFMMFFCalcStopLevel "0" 
+        .SetFMMFFCalcNumInterpPoints "6" 
+        .UseFMMFarfieldCalc "True" 
+        .SetCFIEAlpha "0.500000" 
+        .LowFrequencyStabilization "False" 
+        .LowFrequencyStabilizationML "True" 
+        .Multilayer "False" 
+        .SetiMoMACC_I "0.0001" 
+        .SetiMoMACC_M "0.0001" 
+        .DeembedExternalPorts "True" 
+        .SetOpenBC_XY "True" 
+        .OldRCSSweepDefintion "False" 
+        .SetRCSOptimizationProperties "True", "100", "0.00001" 
+        .SetAccuracySetting "Custom" 
+        .CalculateSParaforFieldsources "True" 
+        .ModeTrackingCMA "True" 
+        .NumberOfModesCMA "3" 
+        .StartFrequencyCMA "-1.0" 
+        .SetAccuracySettingCMA "Default" 
+        .FrequencySamplesCMA "0" 
+        .SetMemSettingCMA "Auto" 
+        .CalculateModalWeightingCoefficientsCMA "True" 
+        .DetectThinDielectrics "True" 
+    End With
+
+    With FDSolver
+        .Start
+    End With
+    """
+    mws._FlagAsMethod("AddToHistory")
+    mws.AddToHistory(Tag, Command)
+
+
 Frq = [5, 7]  # 工作频率，单位：GHz
 
 
@@ -161,127 +295,11 @@ AddToHistory(mws, "change solver type",
 
 # 开始建模
 BrickBuild(mws, 'Brick1', 'WaveGuided', 'RW', 'Vacuum',
-           Xrange=[-a/2, a/2], Yrange=[-b/2, b/2], Zrange=[-l/2, l/2])
+           Xrange=['-a/2', 'a/2'], Yrange=['-b/2', 'b/2'], Zrange=['-l/2', 'l/2'])
 
 # 选择端口
 PickPort1(mws)
 PickPort2(mws)
+
 # 开始仿真
-sCommand = [
-'Mesh.SetCreator "High Frequency" 
-
-'With FDSolver
-     .Reset 
-     .SetMethod "Tetrahedral", "General purpose" 
-     .OrderTet "Second" 
-     .OrderSrf "First" 
-     .Stimulation "All", "All" 
-     .ResetExcitationList 
-     .AutoNormImpedance "False" 
-     .NormingImpedance "50" 
-     .ModesOnly "False" 
-     .ConsiderPortLossesTet "True" 
-     .SetShieldAllPorts "False" 
-     .AccuracyHex "1e-6" 
-     .AccuracyTet "1e-4" 
-     .AccuracySrf "1e-3" 
-     .LimitIterations "False" 
-     .MaxIterations "0" 
-     .SetCalcBlockExcitationsInParallel "True", "True", "" 
-     .StoreAllResults "False" 
-     .StoreResultsInCache "False" 
-     .UseHelmholtzEquation "True" 
-     .LowFrequencyStabilization "True" 
-     .Type "Auto" 
-     .MeshAdaptionHex "False" 
-     .MeshAdaptionTet "True" 
-     .AcceleratedRestart "True" 
-     .FreqDistAdaptMode "Distributed" 
-     .NewIterativeSolver "True" 
-     .TDCompatibleMaterials "False" 
-     .ExtrudeOpenBC "False" 
-     .SetOpenBCTypeHex "Default" 
-     .SetOpenBCTypeTet "Default" 
-     .AddMonitorSamples "True" 
-     .CalcPowerLoss "True" 
-     .CalcPowerLossPerComponent "False" 
-     .StoreSolutionCoefficients "True" 
-     .UseDoublePrecision "False" 
-     .UseDoublePrecision_ML "True" 
-     .MixedOrderSrf "False" 
-     .MixedOrderTet "False" 
-     .PreconditionerAccuracyIntEq "0.15" 
-     .MLFMMAccuracy "Default" 
-     .MinMLFMMBoxSize "0.3" 
-     .UseCFIEForCPECIntEq "True" 
-     .UseEnhancedCFIE2 "True" 
-     .UseFastRCSSweepIntEq "false" 
-     .UseSensitivityAnalysis "False" 
-     .UseEnhancedNFSImprint "False" 
-     .RemoveAllStopCriteria "Hex"
-     .AddStopCriterion "All S-Parameters", "0.01", "2", "Hex", "True"
-     .AddStopCriterion "Reflection S-Parameters", "0.01", "2", "Hex", "False"
-     .AddStopCriterion "Transmission S-Parameters", "0.01", "2", "Hex", "False"
-     .RemoveAllStopCriteria "Tet"
-     .AddStopCriterion "All S-Parameters", "0.01", "2", "Tet", "True"
-     .AddStopCriterion "Reflection S-Parameters", "0.01", "2", "Tet", "False"
-     .AddStopCriterion "Transmission S-Parameters", "0.01", "2", "Tet", "False"
-     .AddStopCriterion "All Probes", "0.05", "2", "Tet", "True"
-     .RemoveAllStopCriteria "Srf"
-     .AddStopCriterion "All S-Parameters", "0.01", "2", "Srf", "True"
-     .AddStopCriterion "Reflection S-Parameters", "0.01", "2", "Srf", "False"
-     .AddStopCriterion "Transmission S-Parameters", "0.01", "2", "Srf", "False"
-     .SweepMinimumSamples "3" 
-     .SetNumberOfResultDataSamples "1001" 
-     .SetResultDataSamplingMode "Automatic" 
-     .SweepWeightEvanescent "1.0" 
-     .AccuracyROM "1e-4" 
-     .AddSampleInterval "", "", "1", "Automatic", "True" 
-     .AddSampleInterval "", "", "", "Automatic", "False" 
-     .MPIParallelization "False"
-     .UseDistributedComputing "False"
-     .NetworkComputingStrategy "RunRemote"
-     .NetworkComputingJobCount "3"
-     .UseParallelization "True"
-     .MaxCPUs "1024"
-     .MaximumNumberOfCPUDevices "2"
-End With
-
-With IESolver
-     .Reset 
-     .UseFastFrequencySweep "True" 
-     .UseIEGroundPlane "False" 
-     .SetRealGroundMaterialName "" 
-     .CalcFarFieldInRealGround "False" 
-     .RealGroundModelType "Auto" 
-     .PreconditionerType "Auto" 
-     .ExtendThinWireModelByWireNubs "False" 
-     .ExtraPreconditioning "False" 
-End With
-
-With IESolver
-     .SetFMMFFCalcStopLevel "0" 
-     .SetFMMFFCalcNumInterpPoints "6" 
-     .UseFMMFarfieldCalc "True" 
-     .SetCFIEAlpha "0.500000" 
-     .LowFrequencyStabilization "False" 
-     .LowFrequencyStabilizationML "True" 
-     .Multilayer "False" 
-     .SetiMoMACC_I "0.0001" 
-     .SetiMoMACC_M "0.0001" 
-     .DeembedExternalPorts "True" 
-     .SetOpenBC_XY "True" 
-     .OldRCSSweepDefintion "False" 
-     .SetRCSOptimizationProperties "True", "100", "0.00001" 
-     .SetAccuracySetting "Custom" 
-     .CalculateSParaforFieldsources "True" 
-     .ModeTrackingCMA "True" 
-     .NumberOfModesCMA "3" 
-     .StartFrequencyCMA "-1.0" 
-     .SetAccuracySettingCMA "Default" 
-     .FrequencySamplesCMA "0" 
-     .SetMemSettingCMA "Auto" 
-     .CalculateModalWeightingCoefficientsCMA "True" 
-     .DetectThinDielectrics "True" 
-End With'
-]
+FreqSimulation(mws)
